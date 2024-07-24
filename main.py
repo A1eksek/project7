071a74e2a0089698804964416cf24080d5275a54
a1eksek <alexey20080705@gmail.com>
2024-07-24 20:53:11 +0400

    init commit

--- main.py
+++ main.py
@@ -29,8 +29,6 @@
 # except:
 #     print('NotImplementedError')
 
-import copy
-
 # class Prototype(object):
 #     def __init__(self):
 #         self._objects = {}
@@ -86,43 +84,153 @@ import copy
 # duck = prototype.clone('Птица', {'name': 'Duck'})
 # print(type(duck), duck.name)
 
-import copy
-from typing import Dict, Any
+# import copy
+# from typing import Dict, Any
+#
+#
+# class Prototype:
+#     def __init__(self) -> None:
+#         self._objects: Dict[str, Any] = {}
+#
+#     def register(self, name: str, obj: Any) -> None:
+#         self._objects[name] = obj
+#
+#     def unregister(self, name: str) -> None:
+#         del self._objects[name]
+#
+#     def clone(self, name: str, attrs: Dict[str, Any]) -> Any:
+#         obj = copy.deepcopy(self._objects[name])
+#         obj.__dict__.update(attrs)
+#         return obj
+#
+#
+# class Bird:
+#     """Птица"""
+#
+#     def __init__(self, name: str = "") -> None:
+#         self.name = name
+#
+#     # Создаем экземпляр прототипа
+#
+#
+# prototype = Prototype()
+# prototype.register('bird', Bird())
+#
+# # Клонируем и изменяем атрибуты для совы
+# owl = prototype.clone('bird', {'name': 'Owl'})
+# print(type(owl), owl.name)  # <class '_main_.Bird'> Owl
+#
+# # Клонируем и изменяем атрибуты для утки
+# duck = prototype.clone('bird', {'name': 'Duck'})
+# print(type(duck), duck.name)  # <class '_main_.Bird'> Duck
+
+# class WindowBase(object):
+#     def show(self):
+#         raise NotImplementedError()
+#     def hide(self):
+#         raise NotImplementedError
+# class MainWindows(WindowBase):
+#     def show(self):
+#         print('Показать MainWindow')
+#     def hide(self):
+#         print('Спрятать MainWindow')
+# class SettingWindows(WindowBase):
+#     def show(self):
+#         print('Show SettingWindow')
+#     def hide(self):
+#         print('Спрятать SettingWindow')
+# class HelpWindows(WindowBase):
+#     def show(self):
+#         print('Show HelpWindow')
+#     def hide(self):
+#         print('Спрятать HelpWindow')
+#
+# class WindowMediator(object):
+#     def __init__(self):
+#         self.windows = dict.fromkeys(['main', 'setting', 'help'])
+#     def show(self, win):
+#         for window in self.windows.values():
+#             if not window is win:
+#                 window.hide()
+from typing import Dict, Optional
+
+class WindowBase:
+    def show(self) -> None:
+        raise NotImplementedError()
+
+    def hide(self) -> None:
+        raise NotImplementedError()
 
 
-class Prototype:
+class MainWindow(WindowBase):
+    def show(self) -> None:
+        print('Show MainWindow')
+
+    def hide(self) -> None:
+        print('Hide MainWindow')
+
+
+class SettingWindow(WindowBase):
+    def show(self) -> None:
+        print('Show SettingWindow')
+
+    def hide(self) -> None:
+        print('Hide SettingWindow')
+
+
+class HelpWindow(WindowBase):
+    def show(self) -> None:
+        print('Show HelpWindow')
+
+    def hide(self) -> None:
+        print('Hide HelpWindow')
+
+
+class WindowMediator:
     def __init__(self) -> None:
-        self._objects: Dict[str, Any] = {}
+        self.windows: Dict[str, Optional[WindowBase]] = {
+            'main': None,
+            'setting': None,
+            'help': None
+        }
 
-    def register(self, name: str, obj: Any) -> None:
-        self._objects[name] = obj
+    def show(self, win: WindowBase) -> None:
+        for window in self.windows.values():
+            if window is not None and window is not win:
+                window.hide()
+        win.show()
 
-    def unregister(self, name: str) -> None:
-        del self._objects[name]
+    def set_main(self, win: MainWindow) -> None:
+        self.windows['main'] = win
 
-    def clone(self, name: str, attrs: Dict[str, Any]) -> Any:
-        obj = copy.deepcopy(self._objects[name])
-        obj.__dict__.update(attrs)
-        return obj
+    def set_setting(self, win: SettingWindow) -> None:
+        self.windows['setting'] = win
 
-
-class Bird:
-    """Птица"""
-
-    def __init__(self, name: str = "") -> None:
-        self.name = name
-
-    # Создаем экземпляр прототипа
+    def set_help(self, win: HelpWindow) -> None:
+        self.windows['help'] = win
 
 
-prototype = Prototype()
-prototype.register('bird', Bird())
+# Создаем окна
+main_win = MainWindow()
+setting_win = SettingWindow()
+help_win = HelpWindow()
 
-# Клонируем и изменяем атрибуты для совы
-owl = prototype.clone('bird', {'name': 'Owl'})
-print(type(owl), owl.name)  # <class '_main_.Bird'> Owl
+# Создаем медиатор и устанавливаем окна
+med = WindowMediator()
+med.set_main(main_win)
+med.set_setting(setting_win)
+med.set_help(help_win)
 
-# Клонируем и изменяем атрибуты для утки
-duck = prototype.clone('bird', {'name': 'Duck'})
-print(type(duck), duck.name)  # <class '_main_.Bird'> Duck
+# Показать главное окно
+main_win.show()  # Show MainWindow
 
+# Показать окно настроек
+med.show(setting_win)
+# Hide MainWindow
+# Show SettingWindow
+
+# Показать окно помощи
+med.show(help_win)
+# Hide MainWindow
+# Hide SettingWindow
+# Show HelpWindow
